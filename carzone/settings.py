26a7ed8788afc5b8799f443683b85abcc09a30a6
Rainstreamweb DP <dhruv@rainstreamweb.com>
2021-12-11 19:48:34 +0530

    debug true change

--- carzone/settings.py
+++ carzone/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*^ru^j#e5jbnm*tex-9)6er(_jh)4rc!i)ar818-pxc=aq1na&/'
 # SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["carzonpro.herokuapp.com","localhost"]
 # ALLOWED_HOSTS = []
